fd1fae36b2ec195f6dc3202b64ff6a1713190358
Han Xiao <han.xiao@zalando.de>
2017-12-19 15:35:38 +0100

    restructure the code

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ from utils.config import AppConfig, ModelParameter
 APP_CONFIG = AppConfig.load_config('config/app.yaml')['default']
 MODEL_CONFIG = APP_CONFIG.model_parameter  # type: ModelParameter
 LOGGER = APP_CONFIG.logger  # type: Logger
+
+LOGGER.info('test')
