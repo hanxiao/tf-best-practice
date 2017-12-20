411abb026cbefc36132543e22a8a38453b78f2e4
Han Xiao <han.xiao@zalando.de>
2017-12-20 21:44:41 +0100

    update yaml

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-import logging
+from logging import Logger
 
 from utils.parameter import AppConfig, ModelParameter
 
-APP_CONFIG = AppConfig.load_config('settings/app.yaml')['default']
+APP_CONFIG = AppConfig.load_config('settings/app.yaml')['debug']
 MODEL_CONFIG = APP_CONFIG.model_parameter  # type: ModelParameter
-LOGGER = APP_CONFIG.logger  # type: logging.Logger
+LOGGER = APP_CONFIG.logger  # type: Logger
