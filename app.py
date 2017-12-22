dc08b735f289e4e96500bcc5ae2b37e6159d9b17
Han Xiao <han.xiao@zalando.de>
2017-12-23 00:33:57 +0100

    add nade model

--- app.py
+++ app.py
@@ -6,4 +6,7 @@ from utils.reader import InputData
 if __name__ == "__main__":
     model = NADE(InputData())
     sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
     print(sess.run(model.logp_loss))
+    print(sess.run(model.xentropy_loss))
+    print(sess.run(model.X_sampled))
