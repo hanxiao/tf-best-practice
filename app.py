13ac22fadbdea3f6a7a8a1f6ba097adba75b3b6c
Han Xiao <han.xiao@zalando.de>
2017-12-22 16:50:34 +0100

    move test*.py to gitignore

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     data = DataReader()
     sess = tf.Session()
     data.init_train_data_op(sess)
-    for _ in range(10):
+    for _ in range(11):
         print(sess.run([data.X_s, data.X_r, data.X_u]))
     pass
