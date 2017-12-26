f5c7c539e1fe94986a24c19423e5c7e05e722bae
Han Xiao <han.xiao@zalando.de>
2017-12-26 13:27:40 +0100

    change aws settings

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ from logging import Logger
 
 from utils.parameter import AppConfig, ModelParameter
 
-APP_CONFIG = AppConfig('settings/app.yaml', 'debug')
+APP_CONFIG = AppConfig('settings/app.yaml', 'aws')
 MODEL_PARAM = APP_CONFIG.model_parameter  # type: ModelParameter
 LOGGER = APP_CONFIG.logger  # type: Logger
