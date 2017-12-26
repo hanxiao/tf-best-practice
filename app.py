2362faaf2547f555f76767b9045a22ffd1fdeea4
Han Xiao <han.xiao@zalando.de>
2017-12-26 13:40:30 +0100

    update debug settings

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import itertools
 import tensorflow as tf
 from tensorflow.contrib.learn import ModeKeys
 
-from config import MODEL_PARAM
+from config import MODEL_PARAM, LOGGER
 from model import nade
 from utils.reader import InputData
 
@@ -19,7 +19,7 @@ def main(argv):
     while True:
         model.train(input_fn=lambda: input_data.input_fn(ModeKeys.TRAIN), steps=1000)
         results_gen = model.predict(input_fn=lambda: input_data.input_fn(ModeKeys.INFER))
-        print(input_data.decode(list(itertools.islice(results_gen, MODEL_PARAM.infer_batch_size))))
+        LOGGER.info(input_data.decode(list(itertools.islice(results_gen, MODEL_PARAM.infer_batch_size))))
 
 
 if __name__ == "__main__":
