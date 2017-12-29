d61359e6ff095fac25b8f5d9f6e2400499acacec
Han Xiao <han.xiao@zalando.de>
2017-12-29 14:58:43 +0100

    fix parameters

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def main(argv):
     config = AppConfig('settings/config.yaml', argv[1])
     params = ModelParams('settings/params.yaml', argv[2])
     input_data = InputData(config, params)
-    model = tf.estimator.Estimator(model_fn=nade.model_fn, params=params, config=config)
+    model = tf.estimator.Estimator(model_fn=nade.model_fn, params=params)
     while True:
         model.train(input_fn=lambda: input_data.input_fn(ModeKeys.TRAIN), steps=1000)
         results_gen = model.predict(input_fn=lambda: input_data.input_fn(ModeKeys.INFER))
