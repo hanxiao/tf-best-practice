a11287b8b792a01add9b2527ecb7d87b930a2ee4
Han Xiao <artex.xh@gmail.com>
2018-02-25 20:43:29 +0800

    add attention

---
+++ attention/dataio.py
@@ -0,0 +1,196 @@
+import re
+from glob import glob
+from string import punctuation
+
+import dask.bag as db
+import tensorflow as tf
+from ruamel.yaml import YAML
+from tensorflow.contrib.learn import ModeKeys
+from tensorflow.python.data import Dataset
+
+import shared
+from utils.helper import flatten
+from utils.logger import JobContext
+from utils.parameter import AppConfig, ModelParams
+
+
+class DataIO:
+
+    def __init__(self, config: AppConfig, params: ModelParams):
+        # copy yaml to model dir
+        config.copyto(config.model_dir)
+        params.copyto(config.model_dir)
+
+        logger = shared.logger
+
+        self.batch_size = params.batch_size
+        self.num_epoch = params.num_epoch
+        self.pattern = re.compile(r'(\s+|[{}])'.format(re.escape(punctuation)))
+        self.unknown_char_idx = params.reserved_char['unknown']
+        self.start_char_idx = params.reserved_char['start']
+        self.end_char_idx = params.reserved_char['end']
+        self.unknown_lang_idx = params.reserved_char['unknown']
+
+        with JobContext('indexing all codes...'):
+            b = db.read_text([config.data_dir + '*.' + v for v in config.all_langs.values()])
+            tokens = b.map(lambda x: self.tokenize(x)).flatten()
+
+            # get frequent tokens with length > 1
+            freq_tokens = tokens.frequencies().filter(
+                lambda x: len(x[0]) > 1).topk(params.freqword_as_char, lambda x: x[1]).map(lambda x: x[0]).compute()
+
+            # get all characters
+            all_chars = b.flatten().distinct().filter(lambda x: x).compute()
+
+            self.char2int = {c: idx for idx, c in enumerate(all_chars + freq_tokens, start=len(params.reserved_char))}
+            self.lang2int = {c: idx for idx, c in enumerate(config.all_langs.values(), start=len(params.reserved_lang))}
+            self.newline_char = self.char2int['\n']
+
+        with JobContext('computing some statistics...'):
+            self.num_line = b.count().compute()
+            self.num_char = max(self.char2int.values()) + 1
+            self.num_lang = max(self.lang2int.values()) + 1
+            logger.info('# lines: %d' % self.num_line)
+            logger.info('# chars: %d (# reserved: %d)' % (self.num_char, len(params.reserved_char)))
+            logger.info('# langs: %d (# reserved: %d)' % (self.num_lang, len(params.reserved_lang)))
+            logger.info('linebreak idx: %d' % self.newline_char)
+
+        self.dump(config.dataio_path)
+        self.after_init(config, params)
+
+    def after_init(self, config: AppConfig, params: ModelParams):
+        self.int2char = {i: c for c, i in self.char2int.items()}
+        self.int2lang = {i: c for c, i in self.lang2int.items()}
+
+        params.add_hparam('num_char', self.num_char)
+        params.add_hparam('num_lang', self.num_lang)
+
+        with JobContext('building data generator...'):
+            self.build_infer_fn(config, params)
+            self.build_train_fn(config, params)
+
+    def input_fn(self, mode: ModeKeys):
+        return {
+                   ModeKeys.TRAIN:
+                       lambda: self.train_ds.repeat(self.num_epoch).padded_batch(self.batch_size,
+                                                                                 padded_shapes=self.output_shapes),
+                   ModeKeys.EVAL:
+                       lambda: self.eval_ds.padded_batch(self.batch_size, padded_shapes=self.output_shapes),
+                   ModeKeys.INFER: lambda: Dataset.range(1)
+               }[mode]().make_one_shot_iterator().get_next(), None
+
+    def output_fn(self, cur_ln, out_fn, lang):
+        self.infer_cur_ln = cur_ln
+        self.infer_out_fn = out_fn
+        if lang in self.lang2int:
+            self.infer_lang = lang
+        else:
+            raise ValueError('inference %s is not supported!' % lang)
+
+        return self.infer_ds.padded_batch(1,
+                                          padded_shapes=self.infer_output_shapes).make_one_shot_iterator().get_next(), None
+
+    def decode(self, predictions):
+        r = []
+        eof = False
+        for j in predictions[0]:
+            if j == self.end_char_idx:
+                eof = True
+                break
+            elif j == self.newline_char:
+                break
+            elif j == self.start_char_idx:
+                r.append('<START>')
+            elif j == self.unknown_char_idx:
+                r.append('<UNKNOWN>')
+            else:
+                r.append(self.int2char[j])
+        if r and r[-1] != self.newline_char:
+            r.append('\n')
+        return ''.join(r), eof
+
+    def build_train_fn(self, config: AppConfig, params: ModelParams):
+        def gen():
+            file_list = [(w, v) for v in config.all_langs.values() for w in
+                         glob(config.data_dir + '*.' + v, recursive=True)]
+            for f, lang in file_list:
+                with open(f) as fp:
+                    linebreak = list(range(params.context_lines))
+                    context = [[self.start_char_idx]] * params.context_lines
+                    all_lines = fp.readlines()
+                    for line in all_lines:
+                        next_line = self.tokenize_by_keywords(line)
+                        context_line = flatten(context)
+                        yield context_line, next_line, \
+                              len(context_line), len(next_line), \
+                              self.lang2int.get(lang, self.unknown_lang_idx), \
+                              flatten(linebreak)
+
+                        context.append(next_line)
+                        context.pop(0)
+                        linebreak.append(linebreak[-1] + len(next_line))
+                        linebreak = [v - linebreak[0] - 1 for v in linebreak]
+                        linebreak.pop(0)
+
+                    context_line = flatten(context)
+                    yield context_line, [self.end_char_idx], \
+                          len(context_line), 1, \
+                          self.lang2int.get(lang, self.unknown_lang_idx), \
+                          linebreak
+
+        self.output_shapes = ([None], [None], [], [], [], [None])
+        self.output_types = (tf.int32,) * len(self.output_shapes)
+        ds = Dataset.from_generator(generator=gen,
+                                    output_types=self.output_types,
+                                    output_shapes=self.output_shapes).shuffle(
+            buffer_size=params.batch_size * 100)  # type: Dataset
+        self.eval_ds = ds.take(params.num_eval)
+        self.train_ds = ds.skip(params.num_eval)
+
+    def build_infer_fn(self, config: AppConfig, params: ModelParams):
+        def gen():
+            linebreaks = list(range(params.context_lines))
+            context = [[self.start_char_idx]] * params.context_lines
+
+            with open(self.infer_out_fn) as fp:
+                all_lines = [self.tokenize_by_keywords(v) for v in fp.readlines()]
+                if len(all_lines) > 0:
+                    context += all_lines
+                    linebreaks = []
+                    for v in context:
+                        if linebreaks:
+                            linebreaks.append(len(v) + linebreaks[-1])
+                        else:
+                            linebreaks.append(0)
+                    context = context[-params.context_lines:]
+                    linebreaks = [v - linebreaks[-params.context_lines - 1] - 1 for v in linebreaks]
+                    linebreaks = linebreaks[-params.context_lines:]
+
+                context_line = flatten(context)
+
+                shared.logger.info('context: %s' % context)
+
+                yield context_line, len(context_line), \
+                      self.lang2int.get(self.infer_lang, self.unknown_lang_idx), \
+                      linebreaks
+
+        self.infer_output_shapes = ([None], [], [], [None])
+        self.infer_output_types = (tf.int32,) * len(self.infer_output_shapes)
+        self.infer_ds = Dataset.from_generator(generator=gen,
+                                               output_types=self.infer_output_types,
+                                               output_shapes=self.infer_output_shapes)
+
+    def tokenize(self, line):
+        return [p for p in self.pattern.split(line) if p]
+
+    def tokenize_by_keywords(self, line):
+        tokens = self.tokenize(line)
+        kw = self.char2int
+        return flatten([kw.get(t, [kw.get(c, self.unknown_char_idx) for c in t]) for t in tokens])
+
+    def dump(self, fn):
+        yaml = YAML(typ='unsafe', pure=True)
+        yaml.register_class(DataIO)
+        with open(fn, 'w') as fp:
+            yaml.dump(self, fp)
+        shared.logger.info('DataIO is dumped to %s' % fn)
---
+++ attention/seq2seq.py
@@ -0,0 +1,199 @@
+import tensorflow as tf
+from tensorflow.contrib.learn import ModeKeys
+from tensorflow.python.ops.distributions.categorical import Categorical
+from tensorflow.python.ops.rnn import _transpose_batch_time
+from tensorflow.python.ops.rnn_cell_impl import LSTMStateTuple, LSTMCell, GRUCell
+
+from utils.sru import SRUCell
+
+
+def make_var(name, shape):
+    return tf.get_variable(name, shape, initializer=tf.orthogonal_initializer)
+
+
+def normalize_loss(loss, mask, batch_size):
+    return tf.reduce_sum(tf.dynamic_partition(loss, mask, 2)[1]) / batch_size
+
+
+def model_fn(features, labels, mode, params, config):
+    cur_batch_D = params.dim_embed
+
+    hinit_embed = make_var('hinit_ebd', [params.num_lang, params.num_hidden])
+    cinit_embed = make_var('cinit_ebd', [params.num_lang, params.num_hidden])
+    zero_embed = make_var('zero_embed', [params.num_lang, cur_batch_D])
+    char_embd = make_var('char_ebd', [params.num_char, params.dim_embed])
+
+    if mode == ModeKeys.TRAIN or mode == ModeKeys.EVAL:
+        X_c, X_s, L_c, L_s, T, B = features
+        cur_batch_B = tf.shape(X_s)[0]
+        cur_batch_T = tf.shape(X_s)[1]
+        Xs_embd = tf.nn.embedding_lookup(char_embd, X_s, name='ebd_nextline_seq')
+        Xs_ta = tf.TensorArray(size=cur_batch_T, dtype=tf.float32).unstack(
+            _transpose_batch_time(Xs_embd), 'TBD_Formatted_Xs')
+    else:
+        X_c, L_c, T, B = features  # only give the context info
+        cur_batch_T = params.infer_seq_length
+
+    tmp_mask = tf.tile(tf.expand_dims(tf.range(0, tf.shape(X_c)[0]), 1), [1, params.context_lines])
+    br_idx = tf.stack([tmp_mask, B], axis=2)
+
+    make_cell = {
+        'lstm': lambda x: LSTMCell(params.num_hidden, name=x, reuse=False),
+        'sru': lambda x: SRUCell(params.num_hidden, name=x, reuse=False),
+        'gru': lambda x: GRUCell(params.num_hidden, name=x, reuse=False)
+    }[params.cell]
+
+    with tf.variable_scope('InitState'):
+        h_init = tf.nn.embedding_lookup(hinit_embed, T)
+        c_init = tf.nn.embedding_lookup(cinit_embed, T)
+        cell_init_state = {
+            'lstm': lambda: LSTMStateTuple(c_init, h_init),
+            'sru': lambda: h_init,
+            'gru': lambda: h_init,
+        }[params.cell]()
+
+    with tf.variable_scope('Encoder'):
+        # make a list of cells for dilated encoder
+        encoder_cell = make_cell('encoder_cell')
+        encoder_output, last_enc_state = \
+            tf.nn.dynamic_rnn(encoder_cell,
+                              tf.nn.embedding_lookup(char_embd, X_c, name='ebd_context_seq'),
+                              initial_state=cell_init_state,
+                              sequence_length=L_c,
+                              dtype=tf.float32)
+
+    with tf.variable_scope('Attention'):
+        # Create an attention mechanism
+        attention_mechanism = tf.contrib.seq2seq.LuongAttention(
+            params.attention['num_unit'], encoder_output,
+            memory_sequence_length=L_c)
+
+    with tf.variable_scope('Decoder'):
+        # make a new cell for decoder
+        decoder_cell = encoder_cell if params.share_cell else make_cell('decoder_cell')
+        decoder_cell = tf.contrib.seq2seq.AttentionWrapper(decoder_cell, attention_mechanism)
+
+        attention_zero = decoder_cell.zero_state(batch_size=cur_batch_B, dtype=tf.float32)
+
+        last_enc_state = attention_zero.clone(cell_state=last_enc_state)
+
+        zero_step = tf.nn.embedding_lookup(zero_embed, T)
+
+        output_layer_info = {
+            'units': params.num_char,  # this is the size of vocabulary
+            'name': 'out_to_character',
+            # linear 'activation': tf.nn.softmax
+        }
+
+        with tf.variable_scope('Shared_Dense', reuse=False) as dense_layer_scope:
+            # this will be replaced by the cell_output later
+            zeros_placeholder = tf.zeros([1, decoder_cell.output_size])
+            tf.layers.dense(zeros_placeholder, **output_layer_info)
+
+        def get_logits(cell_out):
+            # cell_out should be BxH
+            # dense layer is HxNUM_CHAR
+            # logit output is BxNUM_CHAR
+            with tf.variable_scope(dense_layer_scope, reuse=True):
+                return tf.layers.dense(cell_out, **output_layer_info)
+
+        def get_dist(cell_out):
+            return Categorical(logits=get_logits(cell_out),
+                               name='categorical_dist',
+                               allow_nan_stats=False,
+                               dtype=tf.int32)
+
+        def get_prob(cell_out, obs):
+            # get_dist output is BxNUM_CHAR
+            return get_dist(cell_out).prob(obs)
+
+        with tf.name_scope('NADE'):
+            # use this TA to store sampled character in inference time
+            # and embed vector in training and evaluation time
+            output_ta = tf.TensorArray(size=cur_batch_T,
+                                       dtype=tf.int32 if mode == ModeKeys.INFER else tf.float32)
+
+            def loop_fn(time, cell_output, cell_state, loop_state):
+                emit_output = cell_output  # == None for time == 0
+
+                if cell_output is None:
+                    next_cell_state = last_enc_state
+                    next_step = zero_step
+                    next_loop_state = output_ta
+                else:  # pass the last state to the next
+                    next_cell_state = cell_state
+                    if mode == ModeKeys.TRAIN or mode == ModeKeys.EVAL:
+                        next_step = Xs_ta.read(time - 1)
+                        next_loop_state = loop_state.write(time - 1, next_step)
+                    else:
+                        next_symbol = get_dist(cell_output).sample()
+                        next_step = tf.nn.embedding_lookup(char_embd, next_symbol)
+                        next_loop_state = loop_state.write(time - 1, next_symbol)
+
+                if mode == ModeKeys.TRAIN or mode == ModeKeys.EVAL:
+                    elements_finished = (time >= L_s)
+                else:
+                    elements_finished = (time >= cur_batch_T)
+
+                return elements_finished, next_step, next_cell_state, emit_output, next_loop_state
+
+            decoder_emit_ta, _, loop_state_ta = tf.nn.raw_rnn(decoder_cell, loop_fn)
+
+    if mode == ModeKeys.TRAIN or mode == ModeKeys.EVAL:
+        target_mask = tf.sequence_mask(L_s, dtype=tf.int32)
+        batch_size = tf.to_float(cur_batch_B)
+        decoder_out = _transpose_batch_time(decoder_emit_ta.stack())
+
+        xentropy_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=X_s, logits=get_logits(decoder_out))
+        logp_loss = -tf.log(1e-6 + get_prob(decoder_out, X_s))
+
+        if params.loss == 'xentropy':
+            model_loss = xentropy_loss
+            aux_loss = logp_loss
+        elif params.loss == 'logp':
+            model_loss = logp_loss
+            aux_loss = xentropy_loss
+        else:
+            raise NotImplementedError
+
+        model_loss = normalize_loss(model_loss, target_mask, batch_size)
+        aux_loss = normalize_loss(aux_loss, target_mask, batch_size)
+
+        optimizer = {
+            'rmsp': lambda: tf.train.RMSPropOptimizer(learning_rate=params.learning_rate),
+            'adam': lambda: tf.train.AdamOptimizer(learning_rate=params.learning_rate),
+            'sgd': lambda: tf.train.GradientDescentOptimizer(learning_rate=params.learning_rate)
+        }[params.optimizer]()
+
+        if params.gradient_clipping:
+            # Calculate and clip gradients
+            all_params = tf.trainable_variables()
+            gradients = tf.gradients(model_loss, all_params)
+            clipped_gradients, _ = tf.clip_by_global_norm(gradients, params.max_gradient_norm)
+            train_op = optimizer.apply_gradients(zip(clipped_gradients, all_params),
+                                                 global_step=tf.train.get_global_step())
+        else:
+            train_op = optimizer.minimize(loss=model_loss, global_step=tf.train.get_global_step())
+
+        if params.train_loghook:
+            logging_hook = [tf.train.LoggingTensorHook(tensors={'max-idx': tf.reduce_max(X_s),
+                                                                'min-idx': tf.reduce_min(X_s),
+                                                                'shape_x': tf.shape(X_s),
+                                                                'shape_out': tf.shape(decoder_out),
+                                                                'model_loss': model_loss,
+                                                                'aux_loss': aux_loss},
+                                                       every_n_iter=1)]
+        else:
+            logging_hook = None
+
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            loss=model_loss,
+            train_op=train_op,
+            training_hooks=logging_hook)
+    else:
+        X_sampled = _transpose_batch_time(loop_state_ta.stack())
+
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            predictions=X_sampled)
